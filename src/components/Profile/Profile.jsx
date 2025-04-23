12906346c0c8c3d76f0562b2a1db849a06167d7b
Yehor Vahin <davyjohns322@gmail.com>
2025-04-23 12:12:33 +0300

    changed Profile.jsx

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,7 +4,7 @@ const Profile = ({name, tag, location, image, stats}) => {
     return(
         <div className={css.profile}>
             <div className={css.description}>
-                <img alt="User avatar" className={css.avatar}>{image}</img>
+                <img src={image} alt="User avatar" className={css.avatar}/>
                 <p className={css.name}>{name}</p>
                 <p className={css.tag}>@{tag}</p>
                 <p className={css.location}>{location}</p>
